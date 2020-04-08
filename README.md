b0b10366feb7f84273cc325f96275514806d5409
l9ucile <63204731+l9ucile@users.noreply.github.com>
2020-04-08 17:12:37 +0200

    partie B

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ Sleep mode implementation
 
 Pro Micro ATmega32U4 5V 16MHz :
 
-Schematic
-Datasheet
+![im](im.png)
+
+- [ ] [Schematic](https://www.kitronik.co.uk/pdf/46101_schematic.pdf)
+- [ ] [Datasheet](https://www.microchip.com/wwwproducts/en/ATmega32u4)
--- arduino_lowpower.ino
+++ arduino_lowpower.ino
@@ -1,9 +1,23 @@
+const boolean PRESSED = 0;
+const int BUTTONPIN = 1
+const int 
+int counter = 0;
+boolean oppose_value ;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(BUTTONPIN,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (PRESSED = digitalRead(BUTTONPIN) )
+  {
+    while (PRESSED = digitalRead(BUTTONPIN) );
+    oppose_value = digitalRead(BUTTONPIN)
+    delay(1);
 
+
+  }
 }
